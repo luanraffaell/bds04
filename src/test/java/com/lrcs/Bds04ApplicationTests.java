a4c0ba6dd8b3edef1b4fe22321ab03863295f18f
Luan Rafael <luanraffaell@hotmail.com>
2023-01-24 09:25:07 -0300

    project created

---
+++ src/test/java/com/lrcs/Bds04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bds04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
